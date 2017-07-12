f4d56b23f30a43f444b1d1d44a84330f858a44d5
David L <lassiterda@gmail.com>
2017-07-12 14:49:34 -0400

    Routing for Search/Saved complete

--- app/Components/Saved.js
+++ app/Components/Saved.js
@@ -12,12 +12,29 @@ class Saved extends Component {
     window.fetch('/api/saved')
       .then(response => { return response.json() })
       .then(dat => { this.setState({savedArticles: dat}) })
+      .catch(err => { console.log(err) })
   }
 
   render () {
     console.log(this.savedArticles)
     return (
-      <div>Saved Articles</div>
+      <div>
+        <h4>Saved Articles</h4>
+        <div className='row card-panel'>
+          {
+            this.state.savedArticles.length === 0
+              ? <p className='grey-text darken-4 center-align'><i className='material-icons'>mood_bad</i><br />Looks like you dont have any saved articles...</p>
+              : this.state.savedArticles.map(article => {
+                return (
+                  <div key={article.nytId}>
+                    <h5>{article.title}</h5>
+                    <hr />
+                  </div>
+                )
+              })
+          }
+        </div>
+      </div>
     )
   }
 }
--- app/Components/Search.js
+++ app/Components/Search.js
@@ -3,32 +3,63 @@ import React from 'react'
 import ResultsItem from './ResultsItem'
 
 class Search extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      searchTerm: '',
+      startYear: null,
+      endYear: null,
+      results: []
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange (event) {
+    var newState = {}
+    newState[event.target.id] = event.target.value
+    this.setState(newState)
+  }
+
+  handleSubmit () {
+    let queryString = `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=041467f312144c8cb89cdf1c6698a1b7&q=${this.state.searchTerm}`
+
+    window.fetch(queryString)
+      .then(res => { return res.json() })
+      .then(dat => {
+        this.setState({results: dat.response.docs})
+      })
+      .catch(err => console.log(err))
+  }
+
   render () {
     return (
       <div>
+        <h4>Search</h4>
         <div className='row card-panel'>
           <div className='input-field col s12'>
-            <input id='searchTerm' type='text' className='validate' onChange={this.props.handleChange} required />
+            <input id='searchTerm' type='text' className='validate' onChange={this.handleChange} required />
             <label htmlFor='first_name'>Topic</label>
           </div>
           <div className='input-field col s12 m6'>
             <i className='material-icons prefix'>date_range</i>
-            <input id='startYear' type='text' className='validate' onChange={this.props.handleChange} />
+            <input id='startYear' type='text' className='validate' onChange={this.handleChange} />
             <label htmlFor='icon_prefix'>Start Year</label>
           </div>
           <div className='input-field col s12 m6'>
             <i className='material-icons prefix'>date_range</i>
-            <input id='endYear' type='text' className='validate' onChange={this.props.handleChange} />
+            <input id='endYear' type='text' className='validate' onChange={this.handleChange} />
             <label htmlFor='icon_prefix'>End Year</label>
           </div>
           <div className='col  s12'>
             <div className='right-align'>
-              <a className='waves-effect waves-light btn blue darken-3' onClick={this.props.handleSubmit}>Search</a>
+              <a className='waves-effect waves-light btn blue darken-3' onClick={this.handleSubmit}>Search</a>
             </div>
           </div>
         </div>
         <div className='row'>
-          {this.props.articles.map((article, idx) => {
+          <h4>Results</h4>
+          {this.state.results.map((article, idx) => {
             article.key = article._id
             return (
               <div className='col s12 m4'>
